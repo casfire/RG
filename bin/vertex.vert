c0efe96890a372fdbdc2d25769703f5988ad4544
Casfire <admin@casfire.com>
2014-11-06 19:16:09 +0100

    Add GLM

--- bin/vertex.vert
+++ bin/vertex.vert
@@ -1,10 +1,16 @@
 #version 330
 
-layout(location = 0) in vec4 position;
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 color;
+uniform mat4 MVP_m;
+uniform mat4 MVP_v;
+uniform mat4 MVP_p;
+
+smooth out vec4 theColor;
 
 void main() {
 	
-	gl_Position = position;
+	gl_Position = MVP_p * MVP_v * MVP_m * vec4(position, 1.0f);
 	
-	
+	theColor = vec4(color, 1.0f);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,113 @@
 #include "graphics/Shader.hpp"
 #include <SFML/Window.hpp>
 
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/transform.hpp>
+
+
+float xrand(float min, float max) {
+	return min + (max - min) * ((std::rand()) / (float) RAND_MAX);
+}
+
+class Model {
+	std::uint32_t vertexCount;
+	std::vector<float> vertexBuffer;
+	std::vector<std::uint32_t> elementBuffer;
+	GLuint vao, vbo;
+public:
+
+	Model() {
+		vertexCount = 0;
+	}
+	
+	std::uint32_t addVertex(float x, float y, float z, float r, float g, float b) {
+		vertexBuffer.push_back(x);
+		vertexBuffer.push_back(y);
+		vertexBuffer.push_back(z);
+		vertexBuffer.push_back(r);
+		vertexBuffer.push_back(g);
+		vertexBuffer.push_back(b);
+		std::uint32_t ret = vertexCount;
+		vertexCount++;
+		return ret;
+	}
+	
+	void addTriangle(std::uint32_t a, std::uint32_t b, std::uint32_t c) {
+		elementBuffer.push_back(a);
+		elementBuffer.push_back(b);
+		elementBuffer.push_back(c);
+	}
+	
+	void addCube(float x, float y, float z, float sizeX = 1.0, float sizeY = 1.0, float sizeZ = 1.0) {
+		sizeX /= 2.f;
+		sizeY /= 2.f;
+		sizeZ /= 2.f;
+		std::uint32_t a = addVertex(x - sizeX, y - sizeY, z + sizeZ, xrand(0, 1), xrand(0, 1), xrand(0, 1));
+		std::uint32_t b = addVertex(x + sizeX, y - sizeY, z + sizeZ, xrand(0, 1), xrand(0, 1), xrand(0, 1));
+		std::uint32_t c = addVertex(x + sizeX, y - sizeY, z - sizeZ, xrand(0, 1), xrand(0, 1), xrand(0, 1));
+		std::uint32_t d = addVertex(x - sizeX, y - sizeY, z - sizeZ, xrand(0, 1), xrand(0, 1), xrand(0, 1));
+		std::uint32_t e = addVertex(x - sizeX, y + sizeY, z + sizeZ, xrand(0, 1), xrand(0, 1), xrand(0, 1));
+		std::uint32_t f = addVertex(x + sizeX, y + sizeY, z + sizeZ, xrand(0, 1), xrand(0, 1), xrand(0, 1));
+		std::uint32_t g = addVertex(x + sizeX, y + sizeY, z - sizeZ, xrand(0, 1), xrand(0, 1), xrand(0, 1));
+		std::uint32_t h = addVertex(x - sizeX, y + sizeY, z - sizeZ, xrand(0, 1), xrand(0, 1), xrand(0, 1));
+		addTriangle(a, f, b);
+		addTriangle(a, e, f);
+		addTriangle(b, g, c);
+		addTriangle(b, f, g);
+		addTriangle(c, g, d);
+		addTriangle(d, g, h);
+		addTriangle(d, e, a);
+		addTriangle(d, h, e);
+		addTriangle(e, g, f);
+		addTriangle(e, h, g);
+		addTriangle(a, b, c);
+		addTriangle(a, c, d);
+	}
+	
+	void create() {
+		
+		glGenBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertexBuffer.size(), static_cast<const GLvoid*>(&vertexBuffer[0]), GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		
+		glGenBuffers(1, &vao);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vao);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(std::uint32_t) * elementBuffer.size(), static_cast<const GLvoid*>(&elementBuffer[0]), GL_STATIC_DRAW);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		
+	}
+	
+	void draw() {
+		
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vao);
+		
+		glEnableVertexAttribArray(0);
+		glEnableVertexAttribArray(1);
+		
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (GLvoid*) 0);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (GLvoid*) (3 * sizeof(float)));
+		
+		glDrawElements(GL_TRIANGLES, vertexCount, GL_UNSIGNED_INT, (GLvoid*) 0);
+		
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+		
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+	
+
+};
+
 int main() {
 	
 	/* Create window and load OpenGL */
-	sf::Window window(sf::VideoMode(800, 800), "OpenGL", sf::Style::Default);
+	sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default);
 	window.setFramerateLimit(60);
 	if(ogl_LoadFunctions() == ogl_LOAD_FAILED) {
 		std::cout << "Failed to load OpenGL" << std::endl;
@@ -18,22 +121,25 @@ int main() {
 	/* Shaders */
 	GE::Program program = GE::Program(GE::ShaderFile("vertex.vert"), GE::ShaderFile("fragment.frag"));
 	
-	/* Vertex array */
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
+	/* Model */
+	Model model;
+	model.addCube(0, 0, 0, 1, 1, 1);
+	model.create();
 	
-	/* Vertex buffer */
-	const float vertexPositions[] = {
-		0.75f, 0.75f, 0.0f, 1.0f,
-		0.75f, -0.75f,0.0f, 1.0f,
-		-0.75f, -0.75f, 0.0f, 1.0f, };
-	GLuint positionBufferObject;
-	glGenBuffers(1, &positionBufferObject);
-    glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+	/* MVP */
+	glm::mat4 MVP_m = glm::translate(glm::mat4(1.f), glm::vec3(0.f, 0.f, -7.f)) * glm::rotate(0.6f, glm::vec3(1.f, 1.f, 0.f));
+	glm::mat4 MVP_v = glm::mat4(1.0f); // identity
+	glm::mat4 MVP_p = glm::perspective(1.57079632f, window.getSize().x / (float) window.getSize().y, 0.1f, 100.0f);
+	GLuint MVP_m_ID = glGetUniformLocation(program.getID(), "MVP_m");
+	GLuint MVP_v_ID = glGetUniformLocation(program.getID(), "MVP_v");
+	GLuint MVP_p_ID = glGetUniformLocation(program.getID(), "MVP_p");
+	glUseProgram(program.getID());
+	glUniformMatrix4fv(MVP_m_ID, 1, GL_FALSE, glm::value_ptr(MVP_m));
+	glUniformMatrix4fv(MVP_v_ID, 1, GL_FALSE, glm::value_ptr(MVP_v));
+	glUniformMatrix4fv(MVP_p_ID, 1, GL_FALSE, glm::value_ptr(MVP_p));
+	glUseProgram(0);
 	
+	sf::Clock clock;
 	bool run = true;
 	while (run) {
 		sf::Event event;
@@ -53,14 +159,14 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 		
 		glUseProgram(program.getID());
-
-		glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
-
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-
-		glDisableVertexAttribArray(0);
+		
+		/* Update MVP_m */
+		MVP_m = glm::translate(glm::mat4(1.f), glm::vec3(0.f, 0.f, -7.f)) * glm::rotate(clock.getElapsedTime().asSeconds(), glm::vec3(0.f, 1.f, 0.1f));
+		glUniformMatrix4fv(MVP_m_ID, 1, GL_FALSE, glm::value_ptr(MVP_m));
+		
+		/* Draw */
+		model.draw();
+		
 		glUseProgram(0);
 		
 		window.display();
