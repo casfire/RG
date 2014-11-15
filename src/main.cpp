31de6c7623069f98986aad8dc8f8d658153a9eae
Casfire <admin@casfire.com>
2014-11-15 15:08:56 +0100

    Main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "Engine/Engine.hpp"
-
 #include <SFML/Window.hpp>
 #include <iostream>
 
@@ -12,6 +11,7 @@ int main() {
 	/* Create window and OpenGL context */
 	sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default);
 	window.setFramerateLimit(60);
+	window.setVerticalSyncEnabled(true);
 	
 	/* Load OpenGL functions */
 	if (ogl_LoadFunctions() == ogl_LOAD_FAILED) {
@@ -23,11 +23,37 @@ int main() {
 	/* Create asset storage */
 	A::Storage storage;
 	
-	/* Grab and release GLProgram from the storage */
-	A::GLProgram* program1 = storage.grab<A::GLProgram>("assets/program.txt");
-	A::GLProgram* program2 = storage.grab<A::GLProgram>("assets/program.txt");
-	storage.release(program1);
-	storage.release(program2);
+	/* Initialization */
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	A::GLProgram* program = storage.grab<A::GLProgram>("assets/program.txt");	
+	
+	/* Main loop */
+	bool running = true;
+	while (running) {
+		
+		/* Events */
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				running = false;
+				break;
+			case sf::Event::Resized:
+				glViewport(0, 0, event.size.width, event.size.height);
+				break;
+			default: break;
+			}
+		}
+		
+		/* Draw */
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		program->get().bind();
+		
+		program->get().unbind();
+		window.display();
+	}
+	
+	storage.release(program);
 	
 	return 0;
 }
